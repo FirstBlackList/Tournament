d8a627629ff82f19a00e8907d778fb8c7342cf3a
BlackList <azokblack@gmail.com>
2023-03-05 18:54:32 +0400

    _Corrections_

--- src/main/java/ru/netology/manager/ManagerGame.java
+++ src/main/java/ru/netology/manager/ManagerGame.java
@@ -22,25 +22,23 @@ public class ManagerGame {
 
     public int round(String playerName1, String playerName2) {
 
-        Player player_1 = foundIndex(playerName1);
-        Player player_2 = foundIndex(playerName2);
-        if (player_1 == null) {
+        if (foundPlayer(playerName1) == null) {
             throw new NotRegisteredException("The first player is not registered!");
-        } else if (player_2 == null) {
+        } else if (foundPlayer(playerName2) == null) {
             throw new NotRegisteredException("The second player is not registered!");
-        } else if (player_1 == player_2) {
+        } else if (foundPlayer(playerName1) == foundPlayer(playerName2)) {
             throw new AlreadyRegisteredException("A player with that name is already registered");
         }
-        if (player_1.getStrength() > player_2.getStrength()) {
+        if (foundPlayer(playerName1).getStrength() > foundPlayer(playerName2).getStrength()) {
             return 1;
-        } else if (player_1.getStrength() < player_2.getStrength()) {
+        } else if (foundPlayer(playerName1).getStrength() < foundPlayer(playerName2).getStrength()) {
             return 2;
         } else {
             return 0;
         }
     }
 
-    public Player foundIndex(String playerName) {
+    public Player foundPlayer(String playerName) {
         for (Player player : players) {
             if (player.getName().equalsIgnoreCase(playerName)) {
                 return player;
